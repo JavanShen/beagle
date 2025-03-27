fcaae72d5248bbcfd12dbd9e5b2f9b92d00c0ec8
apine <1985486089@qq.com>
2025-03-27 20:14:05 +0800

    🐛 Some music not been parse

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -3,15 +3,10 @@ import { parseWebStream } from "music-metadata";
 import useStore from "@/store";
 
 export const parseID3 = async (filePath: string, signal?: AbortSignal) => {
-  try {
-    const stream = await getFileStream(filePath, signal);
-    const id3 = await parseWebStream(stream);
-    stream.cancel();
-    return id3;
-  } catch (error) {
-    console.log("Error parsing ID3:", error);
-  }
-  return null;
+  const stream = await getFileStream(filePath, signal);
+  const id3 = await parseWebStream(stream);
+  stream.cancel();
+  return id3;
 };
 
 export const parseMusicMeta = async (
@@ -19,29 +14,39 @@ export const parseMusicMeta = async (
   fileName: string,
   signal?: AbortSignal,
 ) => {
-  const { musicMetaMap, musicPath, origin, addMusicMeta } = useStore.getState();
-  if (musicMetaMap.has(sign) || !sign) return;
-  const joinUrl = encodeURI(`${origin}/p${musicPath}/${fileName}?sign=${sign}`);
+  try {
+    const { musicMetaMap, musicPath, origin, addMusicMeta } =
+      useStore.getState();
+    if (musicMetaMap.has(sign) || !sign) return;
+    const joinUrl = encodeURI(
+      `${origin}/p${musicPath}/${fileName}?sign=${sign}`,
+    );
 
-  const id3 = await parseID3(joinUrl, signal);
+    const id3 = await parseID3(joinUrl, signal);
 
-  const { title, artist, picture, album } = id3?.common || {};
-  const coverInfo = picture?.[0];
-  const coverUrl = coverInfo
-    ? URL.createObjectURL(new Blob([coverInfo.data], { type: coverInfo.type }))
-    : undefined;
+    const { title, artist, picture, album } = id3?.common || {};
+    const coverInfo = picture?.[0];
+    const coverUrl = coverInfo
+      ? URL.createObjectURL(
+          new Blob([coverInfo.data], { type: coverInfo.type }),
+        )
+      : undefined;
 
-  const metadata = {
-    title,
-    artist,
-    album,
-    coverUrl,
-    duration: id3?.format.duration,
-    hasMeta: id3 ? true : false,
-    rawUrl: joinUrl,
-  };
+    const metadata = {
+      title,
+      artist,
+      album,
+      coverUrl,
+      duration: id3?.format.duration,
+      hasMeta: id3 ? true : false,
+      rawUrl: joinUrl,
+    };
 
-  addMusicMeta(sign, metadata);
+    addMusicMeta(sign, metadata);
 
-  return metadata;
+    return metadata;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 };
